e1ccec311d5aae91121d2c846b6d995fdfb62c1f
Renan <renan.fachin@hotmail.com>
2025-08-19 21:09:36 -0300

    feat: implements copy url function

--- components/url-list-item.tsx
+++ components/url-list-item.tsx
@@ -1,11 +1,26 @@
+"use client"
+
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { CopyIcon, EyeIcon } from "lucide-react";
+import { Check, CopyIcon, EyeIcon } from "lucide-react";
 import { URLResponse } from "./url-list";
+import { useState } from "react";
 
 export default function UrlListItem({url}: {url: URLResponse}) {
-
+  const [copied, setCopied] = useState(false)
   const shortenUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/${url.shorCode}`
+
+  const handleCopyURL = () => {
+    const fullUrl = `${shortenUrl}`
+
+    navigator.clipboard.writeText(fullUrl).then(() => {
+      setCopied(true)
+
+      setTimeout(() => {
+        setCopied(false)
+      }, 2000)
+    })
+  }
 
   return (
     <li className="bg-neutral-50 p-2 rounded-md flex items-center gap-2 text-sm text-gray-500 justify-between border-neutral-300 border">
@@ -14,8 +29,14 @@ export default function UrlListItem({url}: {url: URLResponse}) {
           </Link>
 
           <div className="flex items-center gap-2">
-            <Button variant="outline" size="icon" className="text-muted-foreground hover:bg-muted cursor-pointer">
-              <CopyIcon className="size-4"/>
+            <Button variant="outline" size="icon" className="text-muted-foreground hover:bg-muted cursor-pointer" onClick={() => handleCopyURL()}>
+              {
+                copied ? (
+                  <Check className="size-4"/>
+                ) : (
+                  <CopyIcon className="size-4"/>
+                )
+              }
               <span className="sr-only">Copy URL</span>
             </Button>
 
